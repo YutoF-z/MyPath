fac210ab068c60a22d3da5dfa4534396efdd613f
YutoF-z <137971325+YutoF-z@users.noreply.github.com>
2026-04-22 19:23:45 +0900

    Refactor LocalPath class for improved metadata handling

--- local/src/commonMain/kotlin/LocalPath.kt
+++ local/src/commonMain/kotlin/LocalPath.kt
@@ -9,16 +9,27 @@ import kotlinx.io.files.SystemFileSystem
 open class LocalPath(
     override val rawPath: String
     @Transient val path: Path = Path(rawPath.stripPrefix())
-    @Transient private var _metadata: FileMetadata? = null
+    _metadata: FileMetadata? = null
 ): MyPath {
-    override suspend fun stat(): LocalPath? = withContext(Dispatchers.IO) { 
-        SystemFileSystem.metadataOrNull(path).also { metadata_ = it } 
+    override suspend fun statOrNull(): LocalPath? {
+        withContext(Dispatchers.IO) { 
+            _metadata = SystemFileSystem.metadataOrNull(path)
+            _metadata ?: return null
+        }
+        return asMyFile() ?: asMyDirectory()
     }
+    override suspend fun stat(): LocalPath = statOrNull() ?: this
 
-    override suspend fun name(): String = path.name
-    override suspend fun metadata(): FileMetadata? = _metadata ?: stat().metadata()
-    open override suspend fun asMyDirectory(): LocalDirectory? = this as? LocalDirectory ?: if(metadata()?.isDirectory ?: false) LocalDirectory(this) else null
-    open override suspend fun asMyFile(): LocalFile? = this as? LocalFile ?: if(metadata()?.isRegularFile ?: false) LocalFile(this) else null
+    override val name: String = path.name
+    override var metadata: FileMetadata? = _metadata
+        private set
+    
+    open override suspend fun asMyDirectory(): LocalDirectory? = 
+        this as? LocalDirectory 
+        ?: if(metadata()?.isDirectory ?: false) LocalDirectory(this) else null
+    open override suspend fun asMyFile(): LocalFile? = 
+        this as? LocalFile 
+        ?: if(metadata()?.isRegularFile ?: false) LocalFile(this) else null
 
     override suspend fun mv(destination: MyPath): MyPath = apply { withContext(Dispatchers.IO) {
         when (destination) {

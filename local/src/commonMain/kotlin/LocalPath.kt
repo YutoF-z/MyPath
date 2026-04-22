56af9224e0911421b316ff90932788b3ee7ac6a8
YutoF-z <137971325+YutoF-z@users.noreply.github.com>
2026-04-22 18:49:38 +0900

    Refactor LocalPath to support suspend functions
    Refactor LocalPath class to use coroutines for file operations and improve metadata handling.

--- local/src/commonMain/kotlin/LocalPath.kt
+++ local/src/commonMain/kotlin/LocalPath.kt
@@ -4,19 +4,29 @@ import kotlinx.io.files.FileMetadata
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 
-
-open class LocalPath(override val rawPath: String): MyPath {
-    val path: Path
-    private val name: String = path.name
-    private var extension: String? = null
-    private var isWritable: Boolean = false
-    private var fileMetadata: FileMetadata? = null
-    
-    
-    override fun stat(): FileMetadata? {
-        stated = true
-        return SystemFileSystem.metadataOrNull(path).also { 
-            fileMetadata = it 
-        }
+@Serializable
+@SerialName("LocalPath")
+open class LocalPath(
+    override val rawPath: String
+    @Transient val path: Path = Path(rawPath.stripPrefix())
+    @Transient private var _metadata: FileMetadata? = null
+): MyPath {
+    override suspend fun stat(): LocalPath? = withContext(Dispatchers.IO) { 
+        SystemFileSystem.metadataOrNull(path).also { metadata_ = it } 
     }
+
+    override suspend fun name(): String = path.name
+    override suspend fun metadata(): FileMetadata? = _metadata ?: stat().metadata()
+    open override suspend fun asMyDirectory(): LocalDirectory? = this as? LocalDirectory ?: if(metadata()?.isDirectory ?: false) LocalDirectory(this) else null
+    open override suspend fun asMyFile(): LocalFile? = this as? LocalFile ?: if(metadata()?.isRegularFile ?: false) LocalFile(this) else null
+
+    override suspend fun mv(destination: MyPath): MyPath = apply { withContext(Dispatchers.IO) {
+        when (destination) {
+            is LocalPath -> SystemFileSystem.atomicMove(path, destination.path)
+            
+        }
+    }}
+    override suspend fun cp(destination: MyPath): MyPath
+    override suspend fun mk(): MyPath
+    override suspend fun rm()
 }

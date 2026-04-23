c94d0cb3c61546c0f0c76fcb9e69215b03513bca
YutoF-z <137971325+YutoF-z@users.noreply.github.com>
2026-04-23 18:50:12 +0900

    Refactor LocalPath class for improved file handling

--- local/src/commonMain/kotlin/LocalPath.kt
+++ local/src/commonMain/kotlin/LocalPath.kt
@@ -3,6 +3,7 @@ package libra.myPath
 import kotlinx.io.files.FileMetadata
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.files.FileNotFoundException
 
 @Serializable
 @SerialName("LocalPath")
@@ -11,32 +12,53 @@ open class LocalPath(
     @Transient val path: Path = Path(rawPath.stripPrefix())
     _metadata: FileMetadata? = null
 ): MyPath {
+    override suspend fun stat(): LocalPath = statOrNull() ?: this
     override suspend fun statOrNull(): LocalPath? {
         withContext(Dispatchers.IO) { 
-            _metadata = SystemFileSystem.metadataOrNull(path)
-            _metadata ?: return null
+            metadata = SystemFileSystem.metadataOrNull(path)
+            metadata ?: return null
         }
         return asMyFile() ?: asMyDirectory()
     }
-    override suspend fun stat(): LocalPath = statOrNull() ?: this
 
     override val name: String = path.name
     override var metadata: FileMetadata? = _metadata
         private set
+
+    open override suspend fun toMyDirectory(): MyDirectory? = 
+        when(metadata()?.isDirectory ?: true) {
+            true -> LocalDirectory(this)
+            false -> null
+        }
+    open override suspend fun toMyFile(): MyFile? = 
+        when(metadata()?.isRegularFile ?: true) {
+            true -> LocalFile(this)
+            false -> null
+        }
     
     open override suspend fun asMyDirectory(): LocalDirectory? = 
         this as? LocalDirectory 
-        ?: if(metadata()?.isDirectory ?: false) LocalDirectory(this) else null
+        ?: when(metadata()?.isDirectory ?: false) {
+            true -> LocalDirectory(this)
+            false -> null
+        }
     open override suspend fun asMyFile(): LocalFile? = 
         this as? LocalFile 
-        ?: if(metadata()?.isRegularFile ?: false) LocalFile(this) else null
-
-    override suspend fun mv(destination: MyPath): MyPath = apply { withContext(Dispatchers.IO) {
-        when (destination) {
-            is LocalPath -> SystemFileSystem.atomicMove(path, destination.path)
-            
+        ?: when(metadata()?.isRegularFile ?: false) {
+            true -> LocalFile(this)
+            false -> null
         }
-    }}
+
+    override suspend fun mv(destination: MyPath): MyPath {
+        statOrNull() ?: throw FileNotFoundException(rawPath)
+        
+        when {
+            metadata()?.isDirectory && 
+            destination is LocalPath -> withContext(Dispatchers.IO) { 
+                SystemFileSystem.atomicMove(path, destination.path)
+            }         
+        }
+    }
     override suspend fun cp(destination: MyPath): MyPath
     override suspend fun mk(): MyPath
     override suspend fun rm()
